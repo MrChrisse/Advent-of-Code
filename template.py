df024e840f225d08dbb159b39cd13a914074d971
Christian Leingang <chrilei2000@gmail.com>
2021-12-11 19:04:06 +0100

    added template

---
+++ template.py
@@ -0,0 +1,22 @@
+# 2d int Array
+lines = [
+    [int(digit) for digit in x]
+    for x in open("2021/Day11/input.txt").read().rsplit("\n")
+]
+
+# 1D String Array
+lines = [x for x in open("2021/Day10/input.txt").read().rsplit("\n")]
+
+
+def sol_1():
+    result_1 = 0
+    return result_1
+
+
+def sol_2():
+    result_2 = 0
+    return result_2
+
+
+print("Solution 1: " + str(sol_1()))
+print("Solution 1: " + str(sol_2()))
