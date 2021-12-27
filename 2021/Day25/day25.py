0fe16b5cb9030df24a4b49e81fe973b4c5a3041c
Christian Leingang <chrilei2000@gmail.com>
2021-12-27 21:09:13 +0100

    finished day25 part1

--- 2021/Day25/day25.py
+++ 2021/Day25/day25.py
@@ -2,25 +2,42 @@ lines = [
     [digit for digit in x]
     for x in open("2021/Day25/test_input.txt").read().rsplit("\n")
 ]
-
-
-def move_east(cur_positions):
-    global lines
-    for row in range(len(lines)):
-        for col in range(len(lines[row])):
-            if lines[row][col + 1] == ".":
-                cur_positions[row][col + 1] = ">"
-                cur_positions[row][col] = "."
-    lines = cur_positions
+cur_positions = lines.copy()
 
 
 def sol_1():
-    result_1 = 0
-    for round in range(100):
-        cur_positions = lines.copy()
-        move_east(cur_positions)
-        move_south()
-    return result_1
+
+    for round in range(1, 1000):
+        moved = False
+        print("Rounds: " + str(round))
+        for i in lines:
+            for j in i:
+                print(j, end="")
+            print()
+        print("Cur_Pos" + str(round))
+
+        for i in cur_positions:
+            for j in i:
+                print(j, end="")
+            print()
+
+        for row in range(len(lines)):
+            for col in range(len(lines[row])):
+                right_element = 0 if col + 1 == len(lines[row]) else col + 1
+                if lines[row][col] == ">" and lines[row][right_element] == ".":
+                    cur_positions[row][right_element] = ">"
+                    cur_positions[row][col] = "."
+                    moved = True
+        for row in range(len(lines)):
+            for col in range(len(lines[row])):
+                below_element = 0 if row + 1 == len(lines) else row + 1
+                if lines[row][col] == "v" and lines[row][below_element] == ".":
+                    cur_positions[below_element][col] = "v"
+                    cur_positions[row][col] = "."
+                    moved = True
+        print(cur_positions == lines)
+        if not moved:
+            return round
 
 
 def sol_2():
